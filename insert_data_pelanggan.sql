1aee3b1ee4e35193fee18be5990668cbc46b2683
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2022-11-14 21:43:25 +0700

    insert data pelanggan & produk

--- insert_data_pelanggan.sql
+++ insert_data_pelanggan.sql
@@ -1,1 +1,12 @@
 INSERT INTO pelanggan (id, nama, telepon, email, alamat) VALUES
+(1, "Farhan", "08580868476", "farhnassegaf@gmail.com", "Teluk jambe Timur, jawa barat"),
+(2, "Assegaf", "02425716305", "assegaffarhan@gmail.com", "Teluk jambe Timur, jawa barat"),
+(3, "Burhan", "085634260779", "burhannudin@gmail.com", "Teluk jambe Barat, jawa barat"),
+(4, "Al", "08535656890700", "alghozali@gmail.com", "Teluk jambe Barat, jawa barat"),
+(5, "Pipit", "0882273767899", "pipitramadhani@gmail.com", "Teluk jambe Timur, jawa barat"),
+(6, "Nisa", "085666667891", "khairunnisa@gmail.com", "Teluk jambe Barat, jawa barat"),
+(7, "Herman", "0853003453998", "hermanuddin@gmail.com", "Teluk jambe Barat, jawa barat"),
+(8, "Yadi", "0851234545389", "yadinurhidayah@gmail.com", "Teluk jambe Timur, jawa barat"),
+(9, "Ujang", "085607050403", "ujangbustomi@gmail.com", "Teluk jambe Barat, jawa barat"),
+(10, "Dudun", "085213797589", "dudunismail@gmail.com", "Teluk jambe Timur, jawa barat");
+
